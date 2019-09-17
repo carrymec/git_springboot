28cfbf9eaaa178075d5475e58a6a86bf1b756ac3
carrymec@126.com <carrymec@126.com>
2019-09-17 14:17:53 +0800

    v5 first commit

--- src/main/java/com/example/git/GitApplication.java
+++ src/main/java/com/example/git/GitApplication.java
@@ -10,6 +10,7 @@ public class GitApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GitApplication.class, args);
+        System.out.println("v5");
     }
 
 }
